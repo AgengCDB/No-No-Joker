8bcf2d099e1d053d64753446d944e3965962c50a
DeathDB1 <ageng.widjaya7@gmail.com>
2024-12-28 11:10:01 +0700

    Change method to destroy pair hand

--- NoNoJoker.lua
+++ NoNoJoker.lua
@@ -22,18 +22,19 @@ SMODS.Joker {
     text = 
     {
       -- Gain +9 Mult if the played hand is a Pair, then destroy the Pair cards.
-      "Gains {C:mult}+#2#{} Mult",
-      "if played hand is",
-      "a {C:attention}Pair{}, destroy",
-      "played hand",
-      "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+      "Gains {C:mult}+#2#{} mult if",
+      "played hand is a {C:attention}Pair{},",
+      "destroy played poker hand",
+      "{C:inactive}(currently {C:mult}+#1#{C:inactive} mult)"
     }
   },
-  config = {extra = { mults = 0, mult_gain = 9 } },
+  config = {extra = { mults = -11, mult_gain = 9 } },
   rarity = 3,
-  cost = 11,
+  cost = 19,
   unlocked = true,
   discovered = true,
+  blueprint_compat = true,                            --does joker work with blueprint
+  eternal_compat = true,                              --can joker be eternal
   atlas = 'NoNoJoker',
   pos = {x = 0, y = 0},
   loc_vars = function(self, info_queue, card)
@@ -42,7 +43,7 @@ SMODS.Joker {
   calculate = function(self, card, context)    
     if context.joker_main then
       return {
-        mult = card.ability.extra.mults,
+        mult_mod = card.ability.extra.mults,
         message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mults } },
         colour = G.C.MULT -- Mult colour
       }
@@ -51,11 +52,30 @@ SMODS.Joker {
       card.ability.extra.tower = {} -- played poker cards
       if context.scoring_name == 'Pair' then   
         card.ability.extra.mults = card.ability.extra.mults + card.ability.extra.mult_gain -- add +Mult to current Mults
-        for i=1, #context.scoring_hand do -- loop through scoring hand
-          if not context.scoring_hand[i].debuff then -- only add non-debuff cards
+        -- * Old code, this code will destroy all scoring hand
+        -- for i=1, #context.scoring_hand do -- loop through scoring hand
+        --   if not context.scoring_hand[i].debuff then -- only add non-debuff cards
+        --     card.ability.extra.tower[#card.ability.extra.tower + 1] = context.scoring_hand[i] -- add card to tower
+        --   end
+        -- end
+
+        -- * This code make sure only joker only destroy "Pair" hand. Made because "Splash Joker" will destroy the full scoring hand.
+
+        -- Count occurrences of each card ID in the scoring hand
+        local id_count = {}
+        for i=1, #context.scoring_hand do
+          local card_id = context.scoring_hand[i]:get_id()
+          id_count[card_id] = (id_count[card_id] or 0) + 1
+        end
+
+        -- Add cards with exactly two occurrences to the extra tower
+        for i=1, #context.scoring_hand do
+          local card_id = context.scoring_hand[i]:get_id()
+          if id_count[card_id] == 2 and not context.scoring_hand[i].debuff then -- only add non-debuff cards
             card.ability.extra.tower[#card.ability.extra.tower + 1] = context.scoring_hand[i] -- add card to tower
           end
         end
+
         return {
           message = 'Upgrade!',
           colour = G.C.MULT, 
